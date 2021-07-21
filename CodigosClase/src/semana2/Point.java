1ba38e6d6d65dfb8a3f8b5ed991bdc31a7b14ed7
unknown <luisa.r574@gmail.com>
2021-07-21 17:56:45 -0500

    ppt semana2 updated

---
+++ CodigosClase/src/semana2/Point.java
@@ -0,0 +1,29 @@
+package semana2;
+
+public class Point {
+
+    private double x;
+    private double y;
+
+    //1
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void setX(double x) {
+        this.x = x;;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
